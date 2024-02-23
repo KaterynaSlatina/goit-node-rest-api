55eb7f80084bbcca04cc39c57f87a75ada8a1d61
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2024-02-23 21:02:48 +0200

    updContactFav

--- app.js
+++ app.js
@@ -3,21 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import contactsRouter from "./routes/contactsRouter.js";
-import mongoose from "mongoose";
-
-const DB_Host = process.env.DB_Host;
-console.log(DB_Host);
-
-// const DB_Host =
-//   "mongodb+srv://Kateryna:TVY2b8EgUFmwGiRk@cluster0.alapfrk.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-mongoose
-  .connect(DB_Host)
-  .then(() => console.log("Database connection successful"))
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+import "./env/env.js";
 
 const app = express();
 
--- env/env.js
+++ env/env.js
@@ -1,13 +1,14 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 
-const DB_Host = process.env.DB_Host;
-
-console.log(DB_Host);
+// const DB_Host = process.env.DB_Host;
+const DB_Host =
+  "mongodb+srv://Kateryna:TVY2b8EgUFmwGiRk@cluster0.alapfrk.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB_Host)
-  .then(() => console.log("Database connection successful"));
-process.exit(0).catch((error) => {
-  console.log(error.message);
-});
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
