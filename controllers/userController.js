550b2b14d4a8e622d18eca44e63958a83599e779
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2024-03-11 18:14:09 +0200

    patch

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,42 @@
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
 import User from "../models/user.js";
+const avatarsDir = path.join("public", "avatars");
 
 export const uploadAvatar = async (req, res, next) => {
   // console.log(req.file);
   try {
-    await fs.rename(
-      req.file.path,
-      path.join(process.cwd(), "public/avatars", req.file.filename)
-    );
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
+    const newFileName = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, newFileName);
 
-    const user = await User.findByIdAndUpdate(
-      req.user.id,
-      { avatar: req.file.filename },
-      { new: true }
-    );
+    await fs.rename(tempUpload, resultUpload);
 
-    if (user === null) {
-      return res.status(401).send({ message: "Not authorized" });
-    }
-    res.send({
-      user: {
-        avatarUrl: user.avatarURL,
-      },
+    const avatarURL = path.join("avatars", newFileName);
+
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    res.json({
+      avatarURL,
     });
+    // await fs.rename(
+    //   req.file.path,
+    //   path.join(process.cwd(), "public/avatars", req.file.filename)
+    // );
+    // const user = await User.findByIdAndUpdate(
+    //   req.user.id,
+    //   { avatar: req.file.filename },
+    //   { new: true }
+    // );
+    // if (user === null) {
+    //   return res.status(401).send({ message: "Not authorized" });
+    // }
+    // res.send({
+    //   user: {
+    //     avatarUrl: user.avatarURL,
+    //   },
+    // });
   } catch (error) {
     next(error);
   }
@@ -39,7 +52,7 @@ export const getAvatar = async (req, res, next) => {
     if (user.avatar === null) {
       return res.status(401).send({ message: "No avatar" });
     }
-    res.sendFile(path.join(process.cwd(), "public/avatars", user.avatarURL));
+    res.sendFile(path.join(process.cwd(), "public", user.avatarURL));
   } catch (error) {
     next(error);
   }
